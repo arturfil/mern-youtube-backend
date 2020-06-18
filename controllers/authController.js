1637563906e401170716ca524312fe064eb97ee5
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2020-06-17 21:44:14 -0400

    cambie las rutas the categoria y algunos metodos the auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,6 +52,18 @@ exports.signout = (req, res) => {
   res.json({message: "Signout success"});
 };
 
+exports.userById = (req, res, next, id) => {
+  User.findById(id).exec((err,user) => {
+    if(err||!user) {
+      return res.status(400).json({
+        error: "User not found"
+      });
+    }
+    req.profile = user;
+    next()
+  });
+}
+
 // exports.isAdmin = (req, res, next) => {
 //   let user = req.profile && req.auth && req.profile._id == req.auth._id
 //     if(!user) {
--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const {userById} = require('../controllers/authController');
 
 const { list, create, remove, categoryById } = require('../controllers/categoryController');
 
 router.get('/categories', list);
-router.post('/create/', create);
+router.post('/create/:userId', create);
 router.delete('/:categoryId/', remove)
 
-router.param('categoryId', categoryById)
+router.param('categoryId', categoryById);
+router.param('userId', userById);
 
 module.exports = router;
